b569b4c883e6662f7022d758b98022ea9ce4a2a4
Ben <rhuang10@mail.sfsu.edu>
2022-12-29 23:15:38 -0800

    added addNode functionality

--- react-trees/src/Components/Tree.jsx
+++ react-trees/src/Components/Tree.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
 import Button from './Button'
+//back button
+//deal with if null cases, empty arrays, etc...
 
 export default class Tree extends Component {
     constructor(props) {
@@ -8,6 +10,7 @@ export default class Tree extends Component {
         this.state = {
             initial: props.data.initial,
             choices: props.data.choices,
+            node: ""
         }
         this.parent = null
     }
@@ -47,6 +50,30 @@ export default class Tree extends Component {
         }
         return this.getChild(data)
     }
+    addNode(key, name, children = null) {
+        const node = {
+            [key]: {
+                name: name,
+                //https://stackoverflow.com/a/40560953
+                //deconstruct {children: [...children] } with spread
+                ...(children && { children: [...children] })
+            }
+        }
+        this.setState((prevState => ({
+            choices: {
+                ...prevState.choices,
+                ...node
+            }
+        })))
+    }
+    handleNodeChange(event) {
+        this.setState({ node: event.target.value })
+    }
+    handleSubmit() {
+        const val = this.state.node.split(",")
+        const children = val[2].split(" ")
+        this.addNode(val[0], val[1], [...children])
+    }
 
     //just need to implement back button
     render() {
@@ -58,6 +85,11 @@ export default class Tree extends Component {
                     name={this.getName(this.getCurrent())}
                     parent={this.parent}
                 />
+                <form >
+                    <label>Add a new node, seperate by comma, <br></br> eg. key, name, children</label><br></br>
+                    <input value={this.state.node} onChange={(e) => this.handleNodeChange(e)} type={"text"} ></input>
+                    <button type='button' onClick={() => this.handleSubmit()} >Submit</button>
+                </form>
                 {this.parent == null ? "" : <button>Back</button>}
             </div >
         )
